14bd88990e9da772f5f541473350fc7e90bff58f
amitex007 <amitex007@gmail.com>
2024-10-05 17:38:36 +0530

    init commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios");
+const { DockerComposeEnvironment } = require("testcontainers");
+
+const app = express();
+const port = 8080;
+
+async function startContainer() {
+  const env = await new DockerComposeEnvironment("./", "docker-compose.yml")
+    .withStartupTimeout(120000)
+    .up();
+  return env;
+}
+
+app.get("/api/call-external", async (req, res) => {
+    let envStart = null;
+  try {
+    try {
+      envStart = await startContainer();
+    } catch (envError) {
+      console.error(envError);
+    }
+    const response = await axios.get("http://localhost:3000/api/data");
+    if(envStart) {
+      await envStart.down();
+    }
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from external API:", error);
+    res.status(500).json({ error: "Failed to fetch data from external API" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
